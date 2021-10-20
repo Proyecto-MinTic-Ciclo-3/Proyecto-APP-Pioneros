46149bd7f318c9fc307ac55dcac24423d1d1b0dd
Juan Diego Calvo h <jdcalvo@utp.edu.co>
2021-10-19 23:19:02 -0500

    refactorizacion codigo autenticacion auth0

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useEffect } from 'react/cjs/react.development';
 import ReactLoading from 'react-loading';
+import {obtenerDatosUsuario} from 'utils/api'
 
 const PrivateRoute = ({children}) => {
     
     const {isAuthenticated,isLoading,loginWithRedirect, getAccessTokenSilently}=useAuth0();
     useEffect(()=>{
         const fetchAuth0Token = async ()=>{
+            //1.Solictar token auth0
             const accessToken= await getAccessTokenSilently({
             audience:'Backend-Api-Autenticacion-Pioneros-Misiontic'
         });
+        //2.Recibir token de auth0
         localStorage.setItem('token', accessToken)
+        await obtenerDatosUsuario(
+            (response)=>{
+                console.log('response',response)
+            },
+            (error)=>{
+                console.error(error)
+            }
+        )
             
         };
         if(isAuthenticated){
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+
+
 console.log(localStorage.getItem('token'))
 const getToken=()=>{
   return `Bearer ${localStorage.getItem('token')}`
@@ -11,7 +13,7 @@ export const obtenerVentas = async (successCallback,errorCallback) => {
     const options = { method: 'GET', 
     url: 'http://localhost:5000/ventas',
     headers:{
-      Authorization:getToken()
+      Authorization:getToken() //3.Enviarle token bakend
     }
    };
     await axios
@@ -100,7 +102,7 @@ export const editarUsuario=async(id,data,successCallback,errorCallback)=>{
   .request(options)
   .then(successCallback)
   .catch(errorCallback);
-}
+};
 export const deleteUsuario=async(data,successCallback,errorCallback)=>{
   const options={
     method: 'DELETE',
@@ -112,5 +114,70 @@ export const deleteUsuario=async(data,successCallback,errorCallback)=>{
   .then(successCallback)
   .catch(errorCallback)
 
+};
+//Obtener datos usuario conectado
+export const obtenerDatosUsuario= async (successCallback,errorCallback) => {
+  const options = { method: 'GET', 
+  url: 'http://localhost:5000/usuarios/self',
+  headers:{
+    Authorization:getToken()
+  }
+ };
+  await axios
+    .request(options)
+    .then(successCallback).catch(errorCallback);
+  
+};
+//API productos
+export const obtenerProductos = async (successCallback,errorCallback) => {
+  const options = { method: 'GET', 
+  url: 'http://localhost:5000/productos',
+  headers:{
+    Authorization:getToken() //3.Enviarle token bakend
+  }
+ };
+  await axios
+    .request(options)
+    .then(successCallback).catch(errorCallback);
+  
+};
+export const crearProducto = async (data,successCallback,errorCallback)=>{
+  const options = {
+      method: 'POST',
+      url:'http://localhost:5000/productos',
+      headers: { 'Content-Type': 'application/json',Authorization:getToken() },
+      data
+  }
+  await axios
+  .request(options)
+  .then(successCallback)
+  .catch(errorCallback);
+
+
+};
+export const editarProducto=async(id,data,successCallback,errorCallback)=>{
+  const options={
+      method: 'PATCH',
+      url: `http://localhost:5000/productos/${id}/`,
+      headers: { 'Content-Type': 'application/json', Authorization:getToken()},
+      data
+    };
+  await axios
+  .request(options)
+  .then(successCallback)
+  .catch(errorCallback);
 }
+export const deleteProducto=async(data,successCallback,errorCallback)=>{
+  const options={
+      method: 'DELETE',
+    url: `http://localhost:5000/productos/${data}`,
+    headers: { 'Content-Type': 'application/json', Authorization:getToken() },      
+  }
+  await axios
+  .request(options)
+  .then(successCallback)
+  .catch(errorCallback)
+
+}
+
 
